403ff67b316a41cb5b90fe1b7475041696757df9
user <user@125.128.151.218>
2020-09-03 22:59:02 +0900

    2020 09 03 정승준 main js 커밋

---
+++ yonginStudyProject/src/main/webapp/resources/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	$(".user_id").click(function(){
+		$(".user_box_con").fadeIn(600);
+	});
+	
+	
+	$(".circle_btn_2").click(function(){
+		$(".user_box_con").fadeOut(600);
+	});
+});
