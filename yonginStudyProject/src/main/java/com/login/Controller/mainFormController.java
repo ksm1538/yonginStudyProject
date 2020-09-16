5ff8bd16c1fb340e09c3f093e055c584c11e97cf
ksm1538 <ksm1538@naver.com>
2020-09-16 15:10:04 +0900

    수정

--- yonginStudyProject/src/main/java/com/login/Controller/mainFormController.java
+++ yonginStudyProject/src/main/java/com/login/Controller/mainFormController.java
@@ -1,18 +1,19 @@
 package com.login.Controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.commonCode.Service.commonCodeService;
+import com.commonCode.VO.commonCodeVO;
 import com.login.Service.loginService;
 import com.login.VO.userInfoVO;
 
@@ -24,15 +25,29 @@ public class mainFormController{
 	@Resource(name="loginService") // 해당 서비스가 리소스임을 표시합니다.
 	private loginService loginService;
 	
+	@Resource(name="commonCodeService")
+	private commonCodeService commonCodeService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(mainFormController.class);
 	
 	/**
 	 * 스터디 관리 Mapping
+	 * @throws Exception 
 	 */
 	@RequestMapping(value = "/mainopen.do", method = RequestMethod.GET)
-	public String studyManagementForm() {
+	public String studyManagementForm(Model model, HttpSession session) throws Exception {
+		/** 세션에 유저가 정상적으로 등록되어 있지 않다면 로그인 페이지로 이동(시작) **/
+		userInfoVO user = (userInfoVO) session.getAttribute("user");
+
+		if(user == null) {
+			return "jsp/login/login";
+		}
+		/** 세션에 유저가 정상적으로 등록되어 있지 않다면 로그인 페이지로 이동(끝) **/
 		
+		List<commonCodeVO> codeResult = commonCodeService.selectCommonCodeList("studyTopic");
+		
+		//model 변수에 데이터를 담아 jsp에 전달
+		model.addAttribute("studyTopicArray", codeResult);
 		return "jsp/main/main";
 	}
 
